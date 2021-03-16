715ee4d71db68bc7efddaf0a9c02b7a7f26ec660
SMYALTAMASH <smy.altamash@gmail.com>
2021-03-16 17:50:12 +0530

    Adding multipliaction and division feature.

--- calculator.py
+++ calculator.py
@@ -1,7 +1,23 @@
 # Objective: create a calculator
+a = int(input('enter first  number:'))
+b = int(input('enter second number:'))
 
 def main():
-    print("Hello Interns")
+    print("This is calculator app")
+    ans=mul()
+    print("the product is",ans)
+    ans=div()
 
+def mul():
+    prod=a*b
+    return prod
+
+def div():
+	try:
+		div=a/b
+		print("division is",div)
+	except :
+		print("cannot be divided by zero")
+    
 if __name__ == "__main__":
     main()
