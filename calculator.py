1c41aed6b1986c05475f8cfadd8009eb43905176
syed <syedadnaan53@gmail.com>
2021-03-16 18:10:09 +0530

    Subtracted two numbers

--- calculator.py
+++ calculator.py
@@ -1,7 +1,10 @@
-# Objective: create a calculator
-
+a = int(input("enter value of a :"))
+b = int(input("enter value of b :"))
 def main():
     print("Hello Interns")
-
+    sub()
+def sub():
+    sub=a-b
+    print("subtraction is",sub)
 if __name__ == "__main__":
     main()
